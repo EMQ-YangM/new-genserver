7a1911e2eb9aa42bdcdffcb93b485ace27b2f34a
EMQ-YangM <yangm@emqx.io>
2022-06-25 03:28:57 +0800

    I found a serialization solution, which was a great success.

--- src/T1.hs
+++ src/T1.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -21,13 +22,17 @@ module T1 where
 import Codec.CBOR.Decoding
 import Codec.CBOR.Encoding
 import Codec.CBOR.Read
-import Codec.CBOR.Write (toLazyByteString)
 import Codec.Serialise
+import Control.Concurrent
+import Control.Monad
+import qualified Data.ByteString.Lazy as LBS
 import Data.Data
+import Data.IORef
 import Data.Kind
 import Data.Maybe (fromMaybe)
 import GHC.Base
 import GHC.TypeLits
+import S
 import Test.QuickCheck
 import Unsafe.Coerce
 
@@ -115,11 +120,157 @@ prop_encode_decode :: K0 -> Property
 prop_encode_decode k@(Sum i _) =
   label ("index is " ++ show i) $
     case deserialiseFromBytes (decode @K0)
-      . toLazyByteString
-      . encode
+      . convertCborEncoder encode
       $ k of
       Left _ -> property False
       Right (_, dk) -> dk === k
+
+class SR1 r a where
+  sender :: IORef (Maybe LBS.ByteString) -> a -> Channel IO LBS.ByteString -> IO ()
+  recver :: IORef (Maybe LBS.ByteString) -> Chan (Sum r) -> Channel IO LBS.ByteString -> IO ()
+
+instance (SCall t req resp :< r, Serialise req, Serialise resp) => SR1 r (SCall t req resp) where
+  sender ref (SCall req mvar) channel@Channel {send} = do
+    lbs <- readIORef ref
+    -- send req
+    send $ convertCborEncoder encode req
+    vdec <- convertCborDecoder decode
+    -- recv resp
+    val <- runDecoderWithChannel channel lbs vdec
+    case val of
+      Left e -> error (show e)
+      Right (v, jl) -> do
+        writeIORef ref jl
+        -- put resp to mvar
+        putMVar mvar v
+
+  recver ref chan channel@Channel {send} = do
+    lbs <- readIORef ref
+    vdec <- convertCborDecoder (decode @req)
+    -- recv req
+    val <- runDecoderWithChannel channel lbs vdec
+    case val of
+      Left e -> error (show e)
+      Right (v, jl) -> do
+        print "server decode req success"
+        writeIORef ref jl
+        mvar <- newEmptyMVar @resp
+        -- write SCall to chan
+        writeChan chan (inject (SCall @t v mvar))
+        -- take mvar, wait resp
+        var <- takeMVar mvar
+        -- resp for req
+        send $ convertCborEncoder encode var
+
+clientHandler ::
+  forall r.
+  Apply (SR1 r) r =>
+  Sum r ->
+  IORef (Maybe LBS.ByteString) ->
+  Channel IO LBS.ByteString ->
+  IO ()
+clientHandler s@(Sum i _) ref channel@Channel {send} = do
+  send $ convertCborEncoder encodeInt i
+  apply @(SR1 r) (\x -> sender @r ref x channel) s
+
+serverHandler ::
+  forall r.
+  (Apply (SR1 r) r) =>
+  IORef (Maybe LBS.ByteString) ->
+  Chan (Sum r) ->
+  Channel IO LBS.ByteString ->
+  IO ()
+serverHandler ref chan channel = do
+  lbs <- readIORef ref
+  vdec <- convertCborDecoder decodeInt
+  val <- runDecoderWithChannel channel lbs vdec
+  case val of
+    Left e -> error (show e)
+    Right (v, jl) -> do
+      writeIORef ref jl
+      apply @(SR1 r)
+        (\(_ :: k) -> recver @r @k ref chan channel)
+        (Sum v undefined :: Sum r)
+
+-------------------------- ee
+data A
+
+type F = Sum '[SCall A Int Bool, SCall A Int Int]
+
+client :: Chan F -> IO ()
+client chan = do
+  mvar <- newEmptyMVar @Bool
+  print "client send req"
+  writeChan chan (inject $ SCall @A (10 :: Int) mvar)
+  resp <- takeMVar mvar
+  print $ "client response val is " ++ show resp
+
+  mvar <- newEmptyMVar @Int
+  print "client send req"
+  writeChan chan (inject $ SCall @A (11 :: Int) mvar)
+  resp <- takeMVar mvar
+  print $ "client response val is " ++ show resp
+
+clientLowHandler :: Chan F -> IORef (Maybe LBS.ByteString) -> Channel IO LBS.ByteString -> IO ()
+clientLowHandler chan ref channel = forever $ do
+  sv <- readChan chan
+  clientHandler sv ref channel
+
+-------------------------- ee
+
+class HandleM a where
+  handleM :: a -> IO ()
+
+instance HandleM (SCall A Int Int) where
+  handleM (SCall i mvar) = do
+    print $ "server recv val " ++ show i
+    putMVar mvar 10010
+    print "server resp val 10010"
+
+instance HandleM (SCall A Int Bool) where
+  handleM (SCall i mvar) = do
+    print $ "server recv val " ++ show i
+    putMVar mvar True
+    print "server resp val True"
+
+server :: Chan F -> IO ()
+server chan = forever $ do
+  sv <- readChan chan
+  apply @HandleM handleM sv
+
+serverLowHandler :: Chan F -> IORef (Maybe LBS.ByteString) -> Channel IO LBS.ByteString -> IO ()
+serverLowHandler chan ref channel = forever $ do
+  serverHandler ref chan channel
+
+-------------------------- ee
+
+example :: IO ()
+example = do
+  (clientChannel, serverChannel) <- createConnectedChannels
+  ------------------------------------
+  clientChan <- newChan
+  clientRef <- newIORef Nothing
+  forkIO $ void $ client clientChan
+  forkIO $ void $ clientLowHandler clientChan clientRef clientChannel
+  ------------------------------------
+  serverChan <- newChan
+  serverRef <- newIORef Nothing
+  forkIO $ void $ server serverChan
+  forkIO $ void $ serverLowHandler serverChan serverRef serverChannel
+  threadDelay 1000
+
+-------------------------- ee
+data Cast msg
+
+data Get resp
+
+data Call req resp
+
+newtype SCast t msg = SCast msg
+
+newtype SGet t resp = SGet (MVar resp)
+
+data SCall t req resp = SCall req (MVar resp)
 
 ---------------------------------------------------------------------------
 apply2 ::
